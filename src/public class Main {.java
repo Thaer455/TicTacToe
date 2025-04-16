731482ff01bde01681a62abbcd1e12475933dccc
Thaer455 <issathaer7@gmail.com>
2025-04-16 10:19:48 +0200

    Add files via upload

---
+++ src/public class Main {.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String[] arr = new String[]{"Ali", "Ahmad", "Belal"};
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
+
